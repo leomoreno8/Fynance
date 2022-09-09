7e03367bfd553819e9efd4689f33004986bb6a73
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2022-09-08 22:04:03 -0300

    Criação do projeto

---
+++ src/test/java/br/edu/utfpr/pb/pw25s/Fynance/FynanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.pw25s.Fynance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FynanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
